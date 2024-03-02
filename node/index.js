a000461e7051d43ae270880a59ebb4b1b8eb4298
Josep Ruiz <josepruizpons@gmail.com>
2024-03-02 15:12:07 +0100

    port

--- node/index.js
+++ node/index.js
@@ -45,7 +45,7 @@ app.use((req, res) => {
     message: 'Unable to find service'
   })
 })
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log(`${'listening for requests on port 3000'}`);
 });
